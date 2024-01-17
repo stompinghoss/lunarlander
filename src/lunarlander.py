12e3fd9f84228194d9a0c41a7fa5c51f837dfe99
fieldp <fieldp@hey.com>
2024-01-17 17:02:45 +0000

    Updated reward function

--- src/lunarlander.py
+++ src/lunarlander.py
@@ -9,6 +9,7 @@ from tqdm import tqdm
 import logging
 import time
 import os
+from custom_reward import LunarLanderCustomReward
 
 '''
 For observation space and reward details, see
@@ -140,70 +141,6 @@ Not firing the engines once in contact with the ground.
 v5.2: added horizontal alignment reward - didn't help. Made slightly worse.
 '''
 
-
-class LunarLanderCustomReward(gym.Wrapper):
-    def __init__(self, env):
-        super().__init__(env)
-
-    def custom_reward(self, state, reward, action):
-        # Constants for the state indices
-        LEFT_LEG_GROUND_CONTACT = 6
-        RIGHT_LEG_GROUND_CONTACT = 7
-        VERTICAL_VELOCITY = 3
-
-        # Constants for actions
-        MAIN_ENGINE = 2
-        LEFT_ENGINE = 1
-        RIGHT_ENGINE = 3
-        ANGLE = 4
-
-        # Engine use penalty when on ground
-        GROUND_ENGINE_USE_PENALTY = -0.4
-        MAIN_ENGINE_USE_PENALTY = -0.6
-        SIDE_ENGINE_USE_PENALTY = -0.06
-        EFFICIENT_DESCENT_REWARD = 0.4
-        ANGLE_THRESHOLD = 0.1  # Smaller values mean stricter control
-        HORIZONTAL_ALIGNMENT_REWARD = 0.1
-        ANGLE_PENALTY = -0.1  # Adjust as needed
-
-        # Reward keeping the craft horizontal
-        lander_angle = state[ANGLE]
-
-        if abs(lander_angle) < ANGLE_THRESHOLD:
-            reward += HORIZONTAL_ALIGNMENT_REWARD
-        else:
-            # Apply penalty based on deviation from horizontal
-            reward += ANGLE_PENALTY * abs(lander_angle)
-
-        # Check if either leg is in contact with the ground
-        ground_contact = (state[LEFT_LEG_GROUND_CONTACT] or
-                          state[RIGHT_LEG_GROUND_CONTACT])
-        side_engine_use = action == LEFT_ENGINE or action == RIGHT_ENGINE
-
-        # Main engine costs more than side engines
-        if action == MAIN_ENGINE:
-            reward += MAIN_ENGINE_USE_PENALTY
-
-        if side_engine_use:
-            reward += SIDE_ENGINE_USE_PENALTY
-
-        # Positive reward if the lander is descending (vertical velocity < 0)
-        # efficiently
-        if state[VERTICAL_VELOCITY] < 0:
-            reward += EFFICIENT_DESCENT_REWARD
-
-        # Penalize if engines are fired while one or both legs on the ground
-        if ground_contact and action == MAIN_ENGINE or side_engine_use:
-            reward += GROUND_ENGINE_USE_PENALTY
-
-        return reward
-
-    def step(self, action):
-        state, reward, terminated, truncated, info = self.env.step(action)
-        reward = self.custom_reward(state, reward, action)
-        return state, reward, terminated, truncated, info
-
-
 def objective(trial):
     start_time = time.time()  # Record the start time of the trial
 
@@ -332,23 +269,6 @@ with open("evaluation_results.txt", "w") as file:
     file.write(f"Mean Reward: {mean_reward}\n")
     file.write(f"Standard Deviation of Reward: {std_reward}\n")
 
-print("Testing the trained agent")
-terminated = False
-truncated = False
-
-for i in range(EVAL_ENV_INTERACTION_STEPS):
-    # Reset the environments at the start of each episode
-    if i == 0 or terminated or truncated:
-        obs = wrapped_env.reset()
-
-    action, _states = model.predict(obs, deterministic=True)
-    obs, rewards, dones, info = wrapped_env.step(action)
-
-    # Extract the action as a single integer value
-    single_action = action[0] if isinstance(action, np.ndarray) else action
-
-    terminated = dones.any()  # Check if any environment is done
-
 wrapped_env.close()
 
-print("Training and evaluation finished.")
+print("Training finished.")
--- src/lunarloader.py
+++ src/lunarloader.py
@@ -1,63 +1,14 @@
 import gymnasium as gym
 from stable_baselines3 import PPO
 from stable_baselines3.common.evaluation import evaluate_policy
-from stable_baselines3.common.env_util import make_vec_env
 import pygame
-from pygame.locals import QUIT
-import numpy as np
 from stable_baselines3.common.monitor import Monitor
-import os
-import time
+from custom_reward import LunarLanderCustomReward
 
 # Define the problem name and other constants
 PROBLEM_NAME = "LunarLander-v2"
 EVAL_EPISODES = 10
 EVAL_ENV_INTERACTION_STEPS = 100000
-
-
-class LunarLanderCustomReward(gym.Wrapper):
-    def __init__(self, env):
-        super().__init__(env)
-
-    def custom_reward(self, state, reward, action):
-        # Constants for the state indices
-        LEFT_LEG_GROUND_CONTACT = 6
-        RIGHT_LEG_GROUND_CONTACT = 7
-        VERTICAL_VELOCITY = 3
-
-        # Constants for actions
-        MAIN_ENGINE = 2
-        LEFT_ENGINE = 1
-        RIGHT_ENGINE = 3
-
-        # Engine use penalty when on ground
-        GROUND_ENGINE_USE_PENALTY = -0.4
-        MAIN_ENGINE_USE_PENALTY = -0.6
-        SIDE_ENGINE_USE_PENALTY = -0.06
-        EFFICIENT_DESCENT_REWARD = 0.2
-
-        # Check if either leg is in contact with the ground
-        ground_contact = state[LEFT_LEG_GROUND_CONTACT] or state[RIGHT_LEG_GROUND_CONTACT]
-        side_engine_use = action == LEFT_ENGINE or action == RIGHT_ENGINE
-
-        # Main engine costs more than side engines
-        if action == MAIN_ENGINE:
-            reward += MAIN_ENGINE_USE_PENALTY
-
-        if side_engine_use:
-            reward += SIDE_ENGINE_USE_PENALTY
-
-        # Reward for efficient descent (using gravity)
-        # Positive reward if the lander is descending (vertical velocity < 0) efficiently
-        if state[VERTICAL_VELOCITY] < 0:
-            reward += EFFICIENT_DESCENT_REWARD
-
-        # Penalize if engines are fired while on ground
-        if ground_contact and action == MAIN_ENGINE or side_engine_use:
-            reward += GROUND_ENGINE_USE_PENALTY
-
-        return reward
-
 
 # Function to load the model
 def load_model():
